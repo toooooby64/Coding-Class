b23108e6c3b6ce9ac59589a2fb98c8f5f58d6573
Jason Alvarez <73498750+toooooby64@users.noreply.github.com>
2022-12-21 15:55:59 -0800

    Update WinterRPG.js

--- WinterRPG.js
+++ WinterRPG.js
@@ -35,3 +35,101 @@ const weapons = [
 		power: 100
 	}
 ];
+
+const monsters = [
+  {
+    name: "slime",
+    level: 2,
+    health: 15
+  },
+  {
+    name: "fanged beast",
+    level: 8,
+    health: 60
+  },
+  {
+    name: "dragon",
+    level: 20,
+    health: 300
+  }
+];
+
+const locations = [
+    {
+        name: "town square",
+        "button text": ["Go to store", "Go to cave", "Fight dragon"],
+        "button functions": [goStore, goCave, fightDragon],
+        text: "You are in the town square. You see a sign that says \"Store.\""
+    },
+	{
+		name: "store",
+		"button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+		"button functions": [buyHealth, buyWeapon, goTown],
+		text: "You enter the store."
+	},
+	{
+		name: "cave",
+		"button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+		"button functions": [fightSlime, fightBeast, goTown],
+		text: "You enter the cave. You see some monsters."
+	},
+	{
+		name: "fight",
+		"button text": ["Attack", "Dodge", "Run"],
+		"button functions": [attack, dodge, goTown],
+		text: "You are fighting a monster."
+	},
+	{
+		name: "kill monster",
+		"button text": ["Go to town square", "Go to town square", "Go to town square"],
+		"button functions": [goTown, goTown, easterEgg],
+		text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+	},
+	{
+		name: "lose",
+		"button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+		"button functions": [restart, restart, restart],
+		text: "You die. ☠️"
+	},
+	{
+		name: "win",
+		"button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+		"button functions": [restart, restart, restart],
+		text: "You defeat the dragon! YOU WIN THE GAME! 🎉"
+    },
+	{
+		name: "easter egg",
+		"button text": ["2", "8", "Go to town square?"],
+		"button functions": [pickTwo, pickEight, goTown],
+		text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
+	}
+]
+
+
+// initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+function update(location) {
+    monsterStats.style.display = "none";
+	button1.innerText = location["button text"][0];
+	button2.innerText = location["button text"][1];
+	button3.innerText = location["button text"][2];
+	button1.onclick = location["button functions"][0];
+	button2.onclick = location["button functions"][1];
+	button3.onclick = location["button functions"][2];
+    text.innerText = location.text;    
+}
+
+function goTown() {
+    update(locations[0]);
+}
+
+function goStore() {
+    update(locations[1]);
+}
+
+function goCave() {
+    update(locations[2]);
+}
