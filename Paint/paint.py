c4b9997be07f3a328aa591306d7fcecd97a3821b
Jason Alvarez <73498750+toooooby64@users.noreply.github.com>
2023-03-21 16:00:33 -0700

    Update paint.py

--- Paint/paint.py
+++ Paint/paint.py
@@ -15,6 +15,7 @@ pink = (255, 192, 203)
 orange = (255, 165, 0)
 yellow = (255, 255, 0)
 violet = (177, 3, 252)
+green = (38,245,45)
 
 penColor = black
 
@@ -35,3 +36,28 @@ col5 = (22, 156, 30, 33)
 col6 = (56, 156, 34, 32)
 col7 = (22, 192, 30, 33)
 col8 = (56, 192, 34, 32)
+
+buttonSelect = (22, 81, 30, 34)
+
+
+def drawRectangle():
+    pygame.gfxdraw.box(drawingWindow, col1, black)
+    pygame.gfxdraw.box(drawingWindow, col2, blue)
+    pygame.gfxdraw.box(drawingWindow, col3, red)
+    pygame.gfxdraw.box(drawingWindow, col4, green)
+    pygame.gfxdraw.box(drawingWindow, col5, pink)
+    pygame.gfxdraw.box(drawingWindow, col6, orange)
+    pygame.gfxdraw.box(drawingWindow, col7, yellow)
+    pygame.gfxdraw.box(drawingWindow, col8, violet)
+
+
+drawRectangle()
+
+pygame.mouse.set_cursor(*pygame.cursors.broken_x)
+exit_game = False
+
+while not exit_game:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      exit_game = True
+pygame.display.update()
