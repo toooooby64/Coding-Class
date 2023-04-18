6cc07c8ca21c2c92d89f3068ea1c603262ea97a6
Jason Alvarez <73498750+toooooby64@users.noreply.github.com>
2023-04-18 16:06:32 -0700

    Update Connect_Four.py

--- Connect_Four.py
+++ Connect_Four.py
@@ -4,14 +4,21 @@ COL_COUNT = 7
 def create_board():
   board = numpy.zeros((6,7))
   return board
+  
 def drop_piece(board,row,col,piece):
   board[row][col] = piece
+  
 def is_valid_location(board, selection):
   return board[5][selection] == 0
+  
 def get_next_open_row(board,selection):
   for r in range(ROW_COUNT):
     if board[r][selection] == 0:
       return r 
+
+def print_board(board):
+  print(numpy.flip(board,0))
+  
 board =  create_board()
 print(board)
 game_over = False
@@ -29,11 +36,10 @@ while not game_over:
     if is_valid_location(board,selection):
       row = get_next_open_row(board,selection)
       drop_piece(board,row,selection,2)
-  print(board)
+  print_board(board)
 
   turn += 1
   turn = turn % 2
-  
   
 
 
